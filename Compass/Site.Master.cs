3f933b08876583ae213b86ff02c00a4fb6891713
Sumant Khera <sumantk@damcogroup.com>
2018-08-06 12:32:12 +0530

    branch and user name displayed using the login user.

--- Compass/Site.Master.cs
+++ Compass/Site.Master.cs
@@ -11,8 +11,25 @@ namespace Compass
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            int userId = 0;
+
+            if (Session["UserId"] == null || !int.TryParse(Session["UserId"].ToString(), out userId))
+            {
+                Session.Abandon();
+                Response.Redirect("/ModuleUI/login.aspx");
+            }
+
             spName.InnerText = Session["UserName"].ToString();
-            spBranch.InnerText = Session["BranchName"].ToString();
+            //Client check
+            if (Session["ClientId"].ToString() != "0")
+            {                
+                spBranch.InnerText = Session["BranchName"].ToString();
+            }
+            else
+            {
+                //Service Company
+                spBranch.InnerText = Session["ServiceCompany"].ToString();
+            }
         }
     }
 }
